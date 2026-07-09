b893254e10b1c50b0d90f9e0836daa0df761ca47
Ahmad Alrefai <ahmad.alrefai018@gmail.com>
2026-07-09 10:36:23 +0000

    Add hexadecimal task

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n + '0');
+
+	for (n = 'a'; n <= 'f'; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
