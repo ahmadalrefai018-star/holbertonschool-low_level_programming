7343f2744f6df998915b0d1a74302741acae8216
Ahmad Alrefai <ahmad.alrefai018@gmail.com>
2026-07-11 07:34:55 +0000

    Add print_number function

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+
+		if (n <= -10)
+			print_number(-(n / 10));
+
+		_putchar(-(n % 10) + '0');
+	}
+	else
+	{
+		if (n >= 10)
+			print_number(n / 10);
+
+		_putchar((n % 10) + '0');
+	}
+}
