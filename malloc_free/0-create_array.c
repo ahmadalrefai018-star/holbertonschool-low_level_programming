ec8173db3f5502e64f3ee7822c3b78ab895fa064
ahmad.alrefai018-star <ahmad,alrefai018@gmail.com>
2026-07-24 14:30:23 +0300

    Fix Betty documentation

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * create_array - creates an array of chars and initializes it
+ * @size: size of the array
+ * @c: character used to fill the array
+ *
+ * Return: pointer to the array, or NULL if size is 0 or malloc fails
+ */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
