72aff937f7ea8be711ede4b9a4810a353cd2c721
ahmad.alrefai018-star <ahmad,alrefai018@gmail.com>
2026-07-24 14:25:52 +0300

    Add malloc_free project

---
+++ malloc_free/0-main.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i % 10)
+			printf(" ");
+		if (!(i % 10) && i)
+			printf("\n");
+		printf("0x%02x", buffer[i]);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	char *buffer;
+
+	buffer = create_array(98, 'H');
+
+	if (buffer == NULL)
+	{
+		printf("Failed to allocate memory\n");
+		return (1);
+	}
+
+	simple_print_buffer(buffer, 98);
+	free(buffer);
+
+	return (0);
+}
