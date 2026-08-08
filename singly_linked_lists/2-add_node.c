cb77121ec6ee790dcf993438ef6fd358300c6dae
ahmad.alrefai018-star <ahmad,alrefai018@gmail.com>
2026-08-08 14:28:40 +0300

    Fix add_node string length

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -24,7 +28,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = strlen(str);
+	new->len = len;
 	new->next = *head;
 	*head = new;
 
