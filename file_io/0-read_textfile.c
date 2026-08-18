1bc0fece20bda3a9a5245c5ebfd38a25dc4335e0
Ahmed Alrefai <ahmad.alrefai018@gmail.com>
2026-08-18 09:47:21 +0300

    Add function that reads and prints a text file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	close(fd);
+	free(buffer);
+
+	if (bytes_written != bytes_read)
+		return (0);
+
+	return (bytes_written);
+}
